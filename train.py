9ea02c5be23ab7f37a83f913316cde862ff1becd
Asmail <asmail790@gmail.com>
2023-06-16 17:54:32 +0200

    changined train parameters and model to small (yolov8s)

--- train.py
+++ train.py
@@ -1,11 +1,19 @@
 from ultralytics import YOLO
 import re
 
-model = YOLO('yolov8n.pt')
+model = YOLO('yolov8s.pt')
 regex = r"model\.[0-9]\..+"
 for k, v in model.model.named_parameters():
     if re.match(regex, k):
         v.requires_grad = False
 
 
-model.train(data='dataset_info.yaml', epochs=10, imgsz=360)
+model.train(
+    data='new_unblock_me_with_grid.yaml',
+    batch=32,
+    epochs=15,
+    imgsz=640,
+    cache=True,
+    workers=4,
+    name='block_dector_with_grid'
+)
