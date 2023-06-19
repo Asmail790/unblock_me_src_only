4d2a07d41b7f3252a9317b12fe88cfb65ae5ee82
Asmail <asmail790@gmail.com>
2023-06-19 17:44:59 +0200

    fixed a bug relateing to order

--- tools/repsentantions/asObjects/utils/converters/instruction.py
+++ tools/repsentantions/asObjects/utils/converters/instruction.py
@@ -19,12 +19,15 @@ class DefaultInstructionConverter(InstructionConverter[MovementDescription]):
 
         old_block = instruction.block
         new_block = type(old_block)(instruction.new_position)
-        blocks = Grid([old_block, new_block])
+        old_grid = Grid([old_block])
+        new_grid = Grid([new_block])
 
-        bounding_boxes = self.__converter.convert_to_grid(blocks)
+        old_bounding_box = self.__converter.convert_to_grid(
+            old_grid).blocks_bounding_boxes[0]
+        new_bounding_box = self.__converter.convert_to_grid(
+            new_grid).blocks_bounding_boxes[0]
+
         message = "Move the " + str(instruction.block.__class__.__name__) + " at position " + str(
             instruction.block.pos) + " to " + str(instruction.new_position)
-        old_bounding_box = bounding_boxes.blocks_bounding_boxes[0]
-        new_bounding_box = bounding_boxes.blocks_bounding_boxes[1]
 
         return NextStep(old_bounding_box, new_bounding_box, message)
